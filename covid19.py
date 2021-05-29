eff316b22ba518418e1e0a1af3a9ad87e635b08e
Muhammet Mert POLAT <mrtplt024@gmail.com>
2021-05-29 20:03:06 +0300

    Update covid19.py

--- covid19.py
+++ covid19.py
@@ -7,21 +7,21 @@ chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-tarayici = webdriver.Chrome('chromedriver', options=chrome_options)
-tarayici.get("https://covid19.saglik.gov.tr/")
+t = webdriver.Chrome('chromedriver', options=chrome_options)
+t.get("https://covid19.saglik.gov.tr/")
 time.sleep(1)
-tarayici.maximize_window()
-tarih = tarayici.find_element_by_css_selector("section#vaka_sayilari_home h3").text
-buguntest = tarayici.find_element_by_css_selector("section#vaka_sayilari_home div:nth-child(2) > div:nth-child(2) > div > p").text
-bugunvaka = tarayici.find_element_by_css_selector("section#vaka_sayilari_home div:nth-child(2) > div:nth-child(3) > div > p").text
-bugunhasta = tarayici.find_element_by_css_selector("section#vaka_sayilari_home div:nth-child(2) > div:nth-child(4) > div > p").text
-bugunvefat = tarayici.find_element_by_css_selector("section#vaka_sayilari_home div:nth-child(2) > div:nth-child(5) > div > p").text
-buguniyilesen = tarayici.find_element_by_css_selector("section#vaka_sayilari_home div:nth-child(2) > div:nth-child(6) > div > p").text
-toplamtest = tarayici.find_element_by_css_selector("section#vaka_sayilari_home div:nth-child(4) > div:nth-child(2) > div > p").text
-toplamvaka = tarayici.find_element_by_css_selector("section#vaka_sayilari_home div:nth-child(4) > div:nth-child(3) > div > p").text
-toplamvefat = tarayici.find_element_by_css_selector("section#vaka_sayilari_home div:nth-child(4) > div:nth-child(4) > div > p").text
-toplamagirhasta = tarayici.find_element_by_css_selector("section#vaka_sayilari_home div:nth-child(4) > div:nth-child(5) > div > p").text
-toplamiyilesen = tarayici.find_element_by_css_selector("section#vaka_sayilari_home div:nth-child(4) > div:nth-child(6) > div > p").text
+t.maximize_window()
+tarih = t.find_element_by_css_selector("section#vaka_sayilari_home h3").text
+buguntest = t.find_element_by_css_selector("section#vaka_sayilari_home div:nth-child(2) > div:nth-child(2) > div > p").text
+bugunvaka = t.find_element_by_css_selector("section#vaka_sayilari_home div:nth-child(2) > div:nth-child(3) > div > p").text
+bugunhasta = t.find_element_by_css_selector("section#vaka_sayilari_home div:nth-child(2) > div:nth-child(4) > div > p").text
+bugunvefat = t.find_element_by_css_selector("section#vaka_sayilari_home div:nth-child(2) > div:nth-child(5) > div > p").text
+buguniyilesen = t.find_element_by_css_selector("section#vaka_sayilari_home div:nth-child(2) > div:nth-child(6) > div > p").text
+toplamtest = t.find_element_by_css_selector("section#vaka_sayilari_home div:nth-child(4) > div:nth-child(2) > div > p").text
+toplamvaka = t.find_element_by_css_selector("section#vaka_sayilari_home div:nth-child(4) > div:nth-child(3) > div > p").text
+toplamvefat = t.find_element_by_css_selector("section#vaka_sayilari_home div:nth-child(4) > div:nth-child(4) > div > p").text
+toplamagirhasta = t.find_element_by_css_selector("section#vaka_sayilari_home div:nth-child(4) > div:nth-child(5) > div > p").text
+toplamiyilesen = t.find_element_by_css_selector("section#vaka_sayilari_home div:nth-child(4) > div:nth-child(6) > div > p").text
 
 print(tarih + "\n" + buguntest + "\n" + bugunvaka + "\n" + bugunhasta + "\n" + bugunvefat + "\n" + buguniyilesen + "\n" + toplamtest + "\n" + toplamvaka + "\n" + toplamvefat + "\n" + toplamagirhasta + "\n" + toplamiyilesen)
 
@@ -43,4 +43,4 @@ data['rapor'].append({
 
 with open('dataset/daily.txt', 'w') as outfile:
     json.dump(data, outfile)
-tarayici.close()
+t.close()
